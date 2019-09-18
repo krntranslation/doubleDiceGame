5ef413b066fdea98db5b2f1e2b5a56719e9ad3ab
unknown <krntranslation@gmail.com>
2019-09-18 14:09:04 -0500

    new update

--- doubleDiceGame.js
+++ doubleDiceGame.js
@@ -1,6 +1,5 @@
-// THREE IS THE MAGIC NUMBER
-//Player to 99 first wins
 
+function runGame(){
 
 // var player1 = prompt("first player name");
 // var player2 = prompt("second player name");
@@ -57,7 +56,8 @@ function scoring(){
       		dice2 = rollDice(8);
       			if(dice1 === dice2){
       				score += 1;
-      			}
+      			prompt("you win!");
+            }
       	}
 
 
@@ -71,94 +71,10 @@ function scoring(){
 
 scoring()
 
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-         // 	let diceFour = rollDice(3);
-       		// let diceSix = rollDice(4);
-       		// let diceNine = rollDice(5);
-       		// let diceTwelve = rollDice(6);
-       		// let diceFourteen = rollDice(7);
-       		// let diceTwenty = rollDice(8);
-        // 	let dices =[diceFour,diceSix,diceNine,diceTwelve,diceFourteen,diceTwenty];
-        // console.log (dices)
-         
-        //  for (i = 0; i < dices.length; i++){
-        //    if(dices[i] 3 === 0){
-        //    score += (dices[i])
-        //       }
-        //    else if(dices[i] % 3 != 0){
-        //    score += (0)
-        //       }          
-        //  }
-        //  console.log (score)
-         
-//          // let playagain = prompt("play again")
-//      		// }
- 
-//  		{
-//  	// prompt("dont u wanna play?")
-// //  		}
-// // 	}
-// // }
-// scoring()
-
-
-// copy of 3 code
-
-// function rollDice(sides){
-//         return Math.floor((Math.random() * sides) + 1);
-// }
-// function scoring(){
-//      let score = 0
-     
-//      for(j = 0; j < 100 && score < 100; j++){
-//       	if(playagain = "y"){   
-//          	let diceFour = rollDice(4);
-//        		let diceSix = rollDice(6);
-//        		let diceNine = rollDice(9);
-//        		let diceTwelve = rollDice(12);
-//        		let diceFourteen = rollDice(14);
-//        		let diceTwenty = rollDice(20);
-//         	let dices =[diceFour,diceSix,diceNine,diceTwelve,diceFourteen,diceTwenty];
-//         console.log (dices)
-         
-//          for (i = 0; i < dices.length; i++){
-//            if(dices[i] % 3 === 0){
-//            score += (dices[i])
-//               }
-//            else if(dices[i] % 3 != 0){
-//            score += (0)
-//               }          
-//          }
-//          console.log (score)
-         
-//          // let playagain = prompt("play again")
-//      		}
- 
-//  		{
-//  	// prompt("dont u wanna play?")
-//  		}
-// 	}
-// }
-// scoring()
 
 
